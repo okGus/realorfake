2960925c25ffdb63bd6d97679504d08daa171ea1
Gustavo <rosalesagustavo@gmail.com>
2021-11-19 12:50:53 -0500

    classify image page

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from flask import render_template, request
 def main():
     if request.method == 'GET':
         return render_template('index.html')
+
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
+    if request.method == 'GET':
+        return render_template('classify_image.html')
+
+    if request.method == 'POST':
+        return render_template('classify_image.html')
