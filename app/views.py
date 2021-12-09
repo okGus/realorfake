418d742efc6c140954418d9c1a12c313dee4c36d
okGus <66441545+okGus@users.noreply.github.com>
2021-12-09 11:26:23 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,19 +1,27 @@
 from app import app
 from flask import render_template, request
 
-import pickle
-from skimage import io
-from skimage import transform
+# from skimage import io
+# from skimage import transform
 
-# path_to_image_classifier = 'models/image-classifier.pkl'
+from werkzeug.utils import secure_filename
+from keras.preprocessing import image
+import numpy as np
+from tensorflow.keras import models
+import os
+# import tensorflow as tf
 
-# with open(path_to_image_classifier, 'rb') as f:
-#     image_classifier = pickle.laod(f)
+UPLOAD_FOLDER = './images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+resModel = models.load_model('app/models/resModel.h5')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'GET':
         return render_template('index.html')
+
 
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
@@ -25,21 +33,37 @@ def classify_image():
         file = request.files['file']
 
         if file:
-            # Read the image using skimage
-            img = io.imread(file)
+            filename = secure_filename(file.filename)
+            filename = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+            file.save(filename)
+            test = file.read()
+            print(test)
+            
+            # Read the image using keras preprocessing
+            img = image.load_img(file, target_size=(256, 256, 3))
 
             # Resize the image to match the input of the model will accept
-            img = transform.resize(img, (960, 960))
+            img = image.img_to_array(img)
 
-            # Flatten image
-            img = img.flatten()
+            # Rescale
+            img = img / 255
 
-            # Get prediction of image from classifier
-            predictions = image_classifier.predict([img])
+            # Expand dimensions
+            img = np.expand_dims(img, axis=0)
+
+            # Get prediction of image from ResNet
+            result = resModel.predict(img)[0][0]
 
             # Get the value of the prediction
-            prediction = predictions[0]
+            prediction = (result < 0.5).astype(np.int)
 
-            return render_template('classify_image.html', prediction=str(prediction))
+            # String prediction
+            final_str_pred = "Fake" if prediction == 0 else "Real"
+
+            # Confidence
+            confidence = result * \
+                100 if final_str_pred == "Fake" else (1 - result) * 100
+
+            return render_template('classify_image.html', final_str_pred=final_str_pred, confidence=confidence)
 
         return render_template('classify_image.html')
